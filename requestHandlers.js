5aaa869981a13e6935cfd9aac5fdbc097927c27a
root <root@localhost.localdomain>
2020-07-06 13:52:47 +0900

    feature/post-data

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,29 +1,24 @@
 var handle = {};
 var fs = require("fs");
 
-handle['/'] = function index(response) {
+handle['/'] = function index(response, postData) {
   console.log("Request handler 'index' was called.");
   function res(err, data){
     if(!err){
       response.writeHead(200, {"Content-Type": "text/html"});
       response.write(data);
       response.end();
+    } else {
+      console.log(err.message);
     }
   }
   fs.readFile('public/index.html', res);
 }
 
-handle['/start'] = function start(response) {
-  console.log("Request handler 'start' was called.");
+handle['/post'] = function post(response, postData) {
+  console.log("Request handler 'post' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("This is Start page");
-  response.end();
-}
-  
-handle['/upload'] = function upload(response) {
-  console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("This is Upload page");
+  response.write(postData);
   response.end();
 }
   
