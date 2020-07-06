8187aa9e8ff332b09c68d8607f5d7610dddf8ca0
root <root@localhost.localdomain>
2020-07-06 15:33:57 +0900

    feature/get-weather

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,11 @@
 var handle = {};
 var fs = require("fs");
+var request = require("request");
+
+var weather = function(city){
+
+
+}
 
 handle['/'] = function index(response, postData) {
   console.log("Request handler 'index' was called.");
@@ -17,9 +23,18 @@ handle['/'] = function index(response, postData) {
 
 handle['/post'] = function post(response, postData) {
   console.log("Request handler 'post' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(postData);
-  response.end();
+  var data = JSON.parse(postData);
+  var base = "https://api.openweathermap.org/data/2.5/weather?";
+  var query = "q=" + data.city + "&units=metric&appid=e753db02de38e5d06f1fdb54a251f5d1";
+  request.get(base + query, function(err, res, body) {
+    if (err) {
+      console.log('Error: ' + err.message);
+      return;
+    }
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(JSON.stringify(body));
+  });
+
 }
   
 exports.handle = handle;
